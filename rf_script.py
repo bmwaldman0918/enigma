ae57a4d5079f3f124183df28f5d4c2d8686971e3
Ben Waldman <bmwaldman0918@gmail.com>
2024-12-07 21:06:20 -0600

    models!

--- rf_script.py
+++ rf_script.py
@@ -7,12 +7,10 @@ import pickle
 file_path = "scraped_data.json"
 raw_data = ""
 with open(file_path, 'r') as f:
-    for i, line in enumerate(f, 1):
-        try:
-            raw_data += line
-        except json.JSONDecodeError as e:
-            print(f"Error parsing JSON on line {i}: {e}")
-df = pd.DataFrame(json.loads(raw_data))
+    try:
+      df = pd.DataFrame(json.loads(f))
+    except Exception as e:
+        print(f"{e}", flush=True)
 clf = RandomForestClassifier()
 X = df["plain"]
 y = df["encoded"]
